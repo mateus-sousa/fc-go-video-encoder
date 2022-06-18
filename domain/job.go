42e84cb1087328db8a413804961a433482210f61
Mateus Silva <mateussilva@ntopus.com.br>
2022-06-18 17:31:14 -0300

    dev - validate and test job

--- domain/job.go
+++ domain/job.go
@@ -1,14 +1,50 @@
 package domain
 
-import "time"
+import (
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
 
 type Job struct {
-	ID               string
-	OutputBucketPath string
-	Status           string
-	Video            *Video
-	VideoID          string
-	Error            string
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
+	ID               string    `valid:"uuid"`
+	OutputBucketPath string    `valid:"notnull"`
+	Status           string    `valid:"notnull"`
+	Video            *Video    `valid:"-"`
+	VideoID          string    `valid:"-"`
+	Error            string    `valid:"-"`
+	CreatedAt        time.Time `valid:"-"`
+	UpdatedAt        time.Time `valid:"-"`
+}
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+func NewJob(output, status string, video *Video) (*Job, error) {
+	job := Job{
+		OutputBucketPath: output,
+		Status:           status,
+		Video:            video,
+	}
+	job.prepare()
+	err := job.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return &job, nil
+}
+
+func (j *Job) prepare() {
+	j.ID = uuid.NewV4().String()
+	j.CreatedAt = time.Now()
+	j.UpdatedAt = time.Now()
+}
+
+func (j *Job) Validate() error {
+	_, err := govalidator.ValidateStruct(j)
+	if err != nil {
+		return err
+	}
+	return nil
 }
