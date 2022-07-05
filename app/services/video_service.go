52822f1156c819092627919e2b201b22579038aa
Mateus Silva <mateussilva@ntopus.com.br>
2022-07-04 22:13:01 -0300

    dev - fragment video

--- app/services/video_service.go
+++ app/services/video_service.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 )
 
 type VideoService struct {
@@ -53,4 +54,30 @@ func (v *VideoService) Download(bucketPath string) error {
 	}
 	log.Printf("video %v has been stored", v.Video.ID)
 	return nil
+}
+
+func (v *VideoService) Fragment() error {
+	err := os.Mkdir(os.Getenv("LOCAL_STORAGE_PATH")+"/"+v.Video.ID+"/", os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	source := os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".mp4"
+	target := os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".frag"
+
+	cmd := exec.Command("mp4fragment", source, target)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+
+	v.printOutput(output)
+	return nil
+}
+
+func (v *VideoService) printOutput(output []byte) {
+	if len(output) > 0 {
+		log.Printf("======> output: %s\n", string(output))
+	}
 }
